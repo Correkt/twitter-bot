edbbc2ade1b53e97c4929d5aede01aa6ede573f5
Ramon Wang <ponmorw@gmail.com>
2023-08-23 19:33:22 -0700

    remove langchain stuff

--- twitter-reply-bot.py
+++ twitter-reply-bot.py
@@ -1,31 +1,30 @@
 import tweepy
 from datetime import datetime, timedelta
-# from langchain.chat_models import ChatOpenAI
-# from langchain.prompts import ChatPromptTemplate, SystemMessagePromptTemplate, HumanMessagePromptTemplate
 import schedule
 import time
 import os
 import pymongo
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
-# Create a new client and connect to the server
-MONGO_CLIENT = MongoClient(MONGO_URI, server_api=ServerApi('1'))
-MONGO_DB = MONGO_CLIENT['correkt']
-bot_collection = MONGO_DB['bot']
 
 # Helpful when testing locally
 from dotenv import load_dotenv
 load_dotenv()
 
 # Load your Twitter and Airtable API keys (preferably from environment variables, config file, or within the railyway app)
-MONGO_URI = os.getenv("MONGO_URI", "YourKey")
-TWITTER_API_KEY = os.getenv("TWITTER_API_KEY", "YourKey")
-TWITTER_API_SECRET = os.getenv("TWITTER_API_SECRET", "YourKey")
-TWITTER_ACCESS_TOKEN = os.getenv("TWITTER_ACCESS_TOKEN", "YourKey")
-TWITTER_ACCESS_TOKEN_SECRET = os.getenv("TWITTER_ACCESS_TOKEN_SECRET", "YourKey")
-TWITTER_BEARER_TOKEN = os.getenv("TWITTER_BEARER_TOKEN", "YourKey")
+MONGO_URI = os.environ["MONGO_URI"]
+TWITTER_API_KEY = os.environ["TWITTER_API_KEY"]
+TWITTER_API_SECRET = os.environ["TWITTER_API_SECRET"]
+TWITTER_ACCESS_TOKEN = os.environ["TWITTER_ACCESS_TOKEN"]
+TWITTER_ACCESS_TOKEN_SECRET = os.environ["TWITTER_ACCESS_TOKEN_SECRET"]
+TWITTER_BEARER_TOKEN = os.environ["TWITTER_BEARER_TOKEN"]
 
-OPENAI_API_KEY = os.getenv("OPENAI_API_KEY", "YourKey")
+MONGO_CLIENT = MongoClient(MONGO_URI, server_api=ServerApi('1'))
+MONGO_DB = MONGO_CLIENT['correkt']
+bot_collection = MONGO_DB['bot']
+bot_collection.create_index([("mentioned_conversation_tweet_id", pymongo.HASHED)])
+
+
 
 # TwitterBot class to help us organize our code and manage shared state
 class TwitterBot:
@@ -38,55 +37,14 @@ class TwitterBot:
                                          wait_on_rate_limit=True)
         self.twitter_me_id = self.get_me_id()
         self.tweet_response_limit = 35 # How many tweets to respond to each time the program wakes up
-
-        # Initialize the language model w/ temperature of .5 to induce some creativity
-        self.llm = ChatOpenAI(temperature=.5, openai_api_key=OPENAI_API_KEY, model_name='gpt-4')
-
-        # For statics tracking for each run. This is not persisted anywhere, just logging
-        self.mentions_found = 0
-        self.mentions_replied = 0
-        self.mentions_replied_errors = 0
-
-    # Generate a response using the language model using the template we reviewed in the jupyter notebook (see README)
-    def generate_response(self, mentioned_conversation_tweet_text):
-        # It would be nice to bring in information about the links, pictures, etc. But out of scope for now
-        # Edit this prompt for your own personality!
-        system_template = """
-            You are an incredibly wise and smart tech mad scientist from silicon valley.
-            Your goal is to give a concise prediction in response to a piece of text from the user.
-            
-            % RESPONSE TONE:
-
-            - Your prediction should be given in an active voice and be opinionated
-            - Your tone should be serious w/ a hint of wit and sarcasm
-            
-            % RESPONSE FORMAT:
-
-            - Respond in under 200 characters
-            - Respond in two or less short sentences
-            - Do not respond with emojis
-            
-            % RESPONSE CONTENT:
-
-            - Include specific examples of old tech if they are relevant
-            - If you don't have an answer, say, "Sorry, my magic 8 ball isn't working right now 🔮"
-        """
-        system_message_prompt = SystemMessagePromptTemplate.from_template(system_template)
-
-        human_template="{text}"
-        human_message_prompt = HumanMessagePromptTemplate.from_template(human_template)
-
-        chat_prompt = ChatPromptTemplate.from_messages([system_message_prompt, human_message_prompt])
-
-        # get a chat completion from the formatted messages
-        final_prompt = chat_prompt.format_prompt(text=mentioned_conversation_tweet_text).to_messages()
-        response = self.llm(final_prompt).content
-        
-        return response
     
-        # Generate a response using the language model
+    # Generate a response
     def respond_to_mention(self, mention, mentioned_conversation_tweet):
-        # response_text = self.generate_response(mentioned_conversation_tweet.text)
+
+        print(mentioned_conversation_tweet)
+
+
+
         response_text = "Hello world!"
         
         # Try and create the response to the tweet. If it fails, log it and move on
@@ -94,9 +52,11 @@ class TwitterBot:
             response_tweet = self.twitter_api.create_tweet(text=response_text, in_reply_to_tweet_id=mention.id)
             self.mentions_replied += 1
         except Exception as e:
-            print (e)
+            print(e)
             self.mentions_replied_errors += 1
             return
+        
+        time.sleep(1)
         
         return True
     
@@ -158,7 +118,7 @@ class TwitterBot:
                 self.respond_to_mention(mention, mentioned_conversation_tweet)
         return True
     
-        # The main entry point for the bot with some logging
+    # The main entry point for the bot with some logging
     def execute_replies(self):
         print (f"Starting Job: {datetime.utcnow().isoformat()}")
         self.respond_to_mentions()
