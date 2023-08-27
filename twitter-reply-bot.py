83c32ecdf39ae2e381a58309b3e87993b3775ddd
mayajagota <mayajagota@ucsb.edu>
2023-08-27 14:06:01 -0700

    changes

--- twitter-reply-bot.py
+++ twitter-reply-bot.py
@@ -8,7 +8,10 @@ import os
 import pymongo
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
+import requests
 # Create a new client and connect to the server
+# MONGO_URI = os.getenv("MONGO_URI", "YourKey")
+MONGO_URI = os.getenv("MONGO_URI")
 MONGO_CLIENT = MongoClient(MONGO_URI, server_api=ServerApi('1'))
 MONGO_DB = MONGO_CLIENT['correkt']
 bot_collection = MONGO_DB['bot']
@@ -18,14 +21,13 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # Load your Twitter and Airtable API keys (preferably from environment variables, config file, or within the railyway app)
-MONGO_URI = os.getenv("MONGO_URI", "YourKey")
 TWITTER_API_KEY = os.getenv("TWITTER_API_KEY", "YourKey")
 TWITTER_API_SECRET = os.getenv("TWITTER_API_SECRET", "YourKey")
 TWITTER_ACCESS_TOKEN = os.getenv("TWITTER_ACCESS_TOKEN", "YourKey")
 TWITTER_ACCESS_TOKEN_SECRET = os.getenv("TWITTER_ACCESS_TOKEN_SECRET", "YourKey")
 TWITTER_BEARER_TOKEN = os.getenv("TWITTER_BEARER_TOKEN", "YourKey")
 
-OPENAI_API_KEY = os.getenv("OPENAI_API_KEY", "YourKey")
+API_KEY = os.getenv("CORREKT_API_KEY")
 
 # TwitterBot class to help us organize our code and manage shared state
 class TwitterBot:
@@ -40,7 +42,7 @@ class TwitterBot:
         self.tweet_response_limit = 35 # How many tweets to respond to each time the program wakes up
 
         # Initialize the language model w/ temperature of .5 to induce some creativity
-        self.llm = ChatOpenAI(temperature=.5, openai_api_key=OPENAI_API_KEY, model_name='gpt-4')
+        # self.llm = ChatOpenAI(temperature=.5, openai_api_key=OPENAI_API_KEY, model_name='gpt-4')
 
         # For statics tracking for each run. This is not persisted anywhere, just logging
         self.mentions_found = 0
@@ -48,47 +50,93 @@ class TwitterBot:
         self.mentions_replied_errors = 0
 
     # Generate a response using the language model using the template we reviewed in the jupyter notebook (see README)
-    def generate_response(self, mentioned_conversation_tweet_text):
-        # It would be nice to bring in information about the links, pictures, etc. But out of scope for now
-        # Edit this prompt for your own personality!
-        system_template = """
-            You are an incredibly wise and smart tech mad scientist from silicon valley.
-            Your goal is to give a concise prediction in response to a piece of text from the user.
-            
-            % RESPONSE TONE:
+    def getImages(mentioned_conversation_tweet):
+        if ('entities' in mentioned_conversation_tweet and 'media' in mentioned_conversation_tweet):
+            media_entities = mentioned_conversation_tweet['entities']['media']
+            images = []
+            for media_entity in media_entities:
+                if (media_entity['type'] == 'photo'):
+                    link = media_entity['url']
+                    images.append(link)
+        return images
 
-            - Your prediction should be given in an active voice and be opinionated
-            - Your tone should be serious w/ a hint of wit and sarcasm
-            
-            % RESPONSE FORMAT:
+    def generate_response(self, mentioned_conversation_tweet):
+        # images = getImages(mentioned_conversation_tweet)
 
-            - Respond in under 200 characters
-            - Respond in two or less short sentences
-            - Do not respond with emojis
-            
-            % RESPONSE CONTENT:
+        # if images:
+        #     data = {"srcs": images, "api_key": API_KEY}
+        #     response = requests.post(url="https://correkt.ai/image", json=data)
+        #     if response.status_code == 200:
+        #         paste = response.json()
+        #         comment = ""
+        #         for i, src in enumerate(images):
+        #             data = paste[src]
+        #             if data["ai_probability"] >= 50:
+        #                 comment += str(i+1) + ") There's a " + str(data["ai_probability"]) + "% chance this is AI generated." +  "\n"
+        #             elif data["ai_probability"] < 50:
+        #                 comment += str(i+1) + ") There is a low chance this is AI generated." + "\n"
+        #             else:
+        #                 comment += str(i+1) + data["error"] + "\n"
+        #         comment += "We are currently in alpha testing. Join our Discord server to learn more! [https://discord.gg/Hj9ffKm9ny]"
+        #     elif response.status_code == 400:
+        #         time.sleep(20)
+        #         data = {"srcs": images, "api_key": API_KEY}
+        #         response = requests.post(url="https://correkt.ai/image", json=data)
+        #         if response.status_code == 200:
+        #             paste = response.json()
+        #             comment = ""
+        #             for i, src in enumerate(images):
+        #                 data = paste[src]
+        #                 if data["ai_probability"] >= 50:
+        #                     comment += str(i+1) + ") There's a " + str(data["ai_probability"]) + "% chance this is AI generated." +  "\n"
+        #                 elif data["ai_probability"] < 50:
+        #                     comment += str(i+1) + ") There is a low chance this is AI generated." + "\n"
+        #                 else:
+        #                     comment += str(i+1) + data["error"] + "\n"
+        #             comment += "\nWe are currently in alpha testing. Join our Discord server to learn more! [https://discord.gg/Hj9ffKm9ny]"
+        #         else:
+        #             print(response.status_code)
+        #     else:
+        #         print(response.status_code)
+        #     time.sleep(1)
 
-            - Include specific examples of old tech if they are relevant
-            - If you don't have an answer, say, "Sorry, my magic 8 ball isn't working right now 🔮"
-        """
-        system_message_prompt = SystemMessagePromptTemplate.from_template(system_template)
+        info = {"api_key": API_KEY, "sentences": [mentioned_conversation_tweet], "parentText": None}
+        response = requests.post(url="https://correkt.ai/validfactcheck", json=info)
+        data = response.json()
+        if (response.status_code == 200):
+            print(1)
+            comment = ""
+            if (data['checkable'] == True):
+                response = requests.post(url="https://correkt.ai/data", json=info)
+                print(response)
+                data = response.json()
+                if (response.status_code == 200):
+                    print(2)
+                    if (data['checkable'] == False):
+                        comment += "The tweet provided could not be fact checked."
+                    # could not extract any claims to be fact checked
+                    elif (data['results'] == "unfounded" or data['results'] == "untrue"):
+                        comment += data["explanation"] + "\n" + data["urls"][0] + data["urls"][1]
+                        print(data["explanation"])
+                # do something with data["urls"] and data["explanation"]
+                    else:
+                # text does not contain misinformation
+                        comment += "The tweet above does not contain misinformation."
+                else:
+                # api call failed, sleep 20 seconds and try this whole process again
+                    time.sleep(20)
+            else:
+        # text is not fact checkable
+                comment += "This tweet is not fact checkable."
+        else:
+            time.sleep(20)
 
-        human_template="{text}"
-        human_message_prompt = HumanMessagePromptTemplate.from_template(human_template)
-
-        chat_prompt = ChatPromptTemplate.from_messages([system_message_prompt, human_message_prompt])
-
-        # get a chat completion from the formatted messages
-        final_prompt = chat_prompt.format_prompt(text=mentioned_conversation_tweet_text).to_messages()
-        response = self.llm(final_prompt).content
-        
-        return response
-    
-        # Generate a response using the language model
+        comment += "\n We are currently in alpha testing. Join us on discord! https://discord.gg/Hj9ffKm9ny"
+        return comment
+ 
     def respond_to_mention(self, mention, mentioned_conversation_tweet):
-        # response_text = self.generate_response(mentioned_conversation_tweet.text)
-        response_text = "Hello world!"
-        
+        response_text = self.generate_response(mentioned_conversation_tweet.text)
+
         # Try and create the response to the tweet. If it fails, log it and move on
         try:
             response_tweet = self.twitter_api.create_tweet(text=response_text, in_reply_to_tweet_id=mention.id)
@@ -98,6 +146,8 @@ class TwitterBot:
             self.mentions_replied_errors += 1
             return
         
+        bot_collection.insert_one({'mentioned_conversation_tweet_id': str(mentioned_conversation_tweet.id)})
+
         return True
     
     # Returns the ID of the authenticated user for tweet creation purposes
@@ -130,7 +180,7 @@ class TwitterBot:
                                                    start_time=start_time_str,
                                                    expansions=['referenced_tweets.id'],
                                                    tweet_fields=['created_at', 'conversation_id']).data
-
+    
     # Checking to see if we've already responded to a mention with what's logged in airtable
     def check_already_responded(self, mentioned_conversation_tweet_id):
         return bot_collection.find_one({'mentioned_conversation_tweet_id': str(mentioned_conversation_tweet_id)}) is not None
@@ -156,6 +206,7 @@ class TwitterBot:
                 and not self.check_already_responded(mentioned_conversation_tweet.id)):
 
                 self.respond_to_mention(mention, mentioned_conversation_tweet)
+
         return True
     
         # The main entry point for the bot with some logging
@@ -172,7 +223,7 @@ def job():
 
 if __name__ == "__main__":
     # Schedule the job to run every 5 minutes. Edit to your liking, but watch out for rate limits
-    schedule.every(6).minutes.do(job)
+    schedule.every(1).minutes.do(job)
     while True:
         schedule.run_pending()
         time.sleep(1)
